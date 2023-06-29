f2991263bf14a005e90c7adcd615679294d31ef3
kutsarvasya <vkutsar89@gmail.com>
2023-06-29 17:37:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="social-network">
+      <BrowserRouter basename="/social-network">
         <App />
       </BrowserRouter>
     </PersistGate>
